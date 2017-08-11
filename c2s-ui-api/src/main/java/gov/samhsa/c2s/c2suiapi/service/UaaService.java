8e2160445628b60d955af9a078d2cfaef7f15f24
JiahaoLi <Jiahao.Li@feisystems.com>
2017-08-11 10:59:39 -0400

    Add login request dto
    #7693: As a patient, I can view my health information from HIE

--- c2s-ui-api/src/main/java/gov/samhsa/c2s/c2suiapi/service/UaaService.java
+++ c2s-ui-api/src/main/java/gov/samhsa/c2s/c2suiapi/service/UaaService.java
@@ -1,6 +1,8 @@
 package gov.samhsa.c2s.c2suiapi.service;
 
 
+import gov.samhsa.c2s.c2suiapi.service.dto.LoginRequestDto;
+
 public interface UaaService {
-    Object login(String username, String password);
+    Object login(LoginRequestDto requestDto);
 }
