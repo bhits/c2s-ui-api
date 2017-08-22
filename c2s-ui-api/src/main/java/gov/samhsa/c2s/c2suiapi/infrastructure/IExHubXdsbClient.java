9c38166639c644af9747464cf5b494b0bdda2e7c
Shruti Rao <shruti.rao@feisystems.com>
2017-08-22 16:30:34 -0400

    Integrate with iexhub-xdsb client
    #15810:As a developer, I will integrate patient UI with backend to get health data from HIE

--- c2s-ui-api/src/main/java/gov/samhsa/c2s/c2suiapi/infrastructure/IExHubXdsbClient.java
+++ c2s-ui-api/src/main/java/gov/samhsa/c2s/c2suiapi/infrastructure/IExHubXdsbClient.java
@@ -8,5 +8,5 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @FeignClient("iexhub-xdsb")
 public interface IExHubXdsbClient {
     @RequestMapping(value = "/patients/{patientId}/health-information", method = RequestMethod.GET)
-    Object getPatientHealthDataFromHIE(@PathVariable("patientId") String patientId);
+    String getPatientHealthDataFromHIE(@PathVariable("patientId") String patientId);
 }
