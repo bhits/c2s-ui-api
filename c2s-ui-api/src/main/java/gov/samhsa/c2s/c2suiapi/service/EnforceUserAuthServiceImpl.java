15a452f65300de040c078dd294578d9aa8504041
Teng Luo <Teng.Luo@feisystems.com>
2017-09-05 15:37:28 -0400

    Change UserAuthIdVerificationException to UmsClientInterfaceException
    Story 15948*:(Technical Debt) Catch FeignException instead of HystrixRuntimeException on newer version of Spring Boot projects

--- c2s-ui-api/src/main/java/gov/samhsa/c2s/c2suiapi/service/EnforceUserAuthServiceImpl.java
+++ c2s-ui-api/src/main/java/gov/samhsa/c2s/c2suiapi/service/EnforceUserAuthServiceImpl.java
@@ -10,6 +10,7 @@ import gov.samhsa.c2s.c2suiapi.service.exception.UserUnauthorizedException;
 import gov.samhsa.c2s.c2suiapi.service.exception.checkedexceptions.AbstractUserAuthIdException;
 import gov.samhsa.c2s.c2suiapi.service.exception.checkedexceptions.UserAuthIdNotFoundException;
 import gov.samhsa.c2s.c2suiapi.service.exception.checkedexceptions.UserAuthIdVerificationException;
+import gov.samhsa.c2s.c2suiapi.service.exception.ums.UmsClientInterfaceException;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -80,7 +81,7 @@ public class EnforceUserAuthServiceImpl implements EnforceUserAuthService {
                     throw new UserAuthIdNotFoundException("No user found for the specified user auth id");
                 default:
                     log.error("UMS client returned an unexpected instance of FeignException", fe);
-                    throw new UserAuthIdVerificationException("An unknown error occurred while attempting to communicate with UMS service");
+                    throw new UmsClientInterfaceException("An unknown error occurred while attempting to communicate with UMS service");
             }
         }
         return userDto;
