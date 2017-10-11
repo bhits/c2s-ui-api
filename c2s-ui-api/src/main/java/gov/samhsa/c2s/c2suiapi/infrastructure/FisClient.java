5cc19f02e6d72e36e6535f5245292a718d95db51
CindyLiFei <cindy.li@feisystems.com>
2017-10-11 16:41:07 -0400

    Implement retrieving patient fhir resource
    # 16090 - As a developer, I will have made the IExHub API implementation FHIR Compatible for Get Health Data by Patient ID

---
+++ c2s-ui-api/src/main/java/gov/samhsa/c2s/c2suiapi/infrastructure/FisClient.java
@@ -0,0 +1,12 @@
+package gov.samhsa.c2s.c2suiapi.infrastructure;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient("fis")
+public interface FisClient {
+    @RequestMapping(value = "/resources", method = RequestMethod.GET)
+    String getPatientResource(@RequestParam(value = "patientIdentifierSystem", required = false) String patientIdentifierSystem, @RequestParam(value = "patientIdentifierValue", required = false) String patientIdentifierValue);
+}
---
+++ c2s-ui-api/src/main/java/gov/samhsa/c2s/c2suiapi/service/FisService.java
@@ -0,0 +1,6 @@
+package gov.samhsa.c2s.c2suiapi.service;
+
+public interface FisService {
+
+    String getPatientResource(String patientIdentifierSystem, String patientIdentifierValue);
+}
